dceb6e44cd6226fcf88edd1190ea1534e38bcf96
Mohammed Shibli <shiblidude27@gmail.com>
2019-11-17 13:55:31 +0530

    added instructions

--- puttest.sh
+++ puttest.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# the script will test for PUT upload method against all the hosts
 
-for domain in $(cat alive.txt)
+#usage:
+# $ ./puttest.sh hosts.txt
+
+for domain in $(cat $1)
 do
 	curl -i -X PUT -d "hello world"  "${domain}/evil.txt"
 done
