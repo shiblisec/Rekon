1cf922423e81484839469d932acd0a0e2e093767
dark-warlord14 <shantanu.ghumade@gmail.com>
2020-01-05 14:02:04 +0530

    Update response.sh
    Solution for issue `Question regarding response.sh #5`

--- response.sh
+++ response.sh
@@ -7,7 +7,8 @@ CURRENT_PATH=$(pwd)
 
 for x in $(cat $1)
 do
-	NAME=$(echo $x | awk -F/ '{print $3}')
-	curl -X GET -H "X-Forwarded-For: evil.com" $x -I > "$CURRENT_PATH/headers/$NAME"
-	curl -s -X GET -H "X-Forwarded-For: evil.com" -L $x > "$CURRENT_PATH/responsebody/$NAME"
+        NAME=$(echo $x | awk -F/ '{print $3}')
+        y=$(echo $x | awk -F/ '{print $1}'| sed 's/.$//')
+        curl -k -X GET -H "X-Forwarded-For: 127.0.0.1" $x -I > "$CURRENT_PATH/headers/$NAME-$y"
+        curl -k -s -X GET -H "X-Forwarded-For: 127.0.0.1" -L $x > "$CURRENT_PATH/responsebody/$NAME-$y"
 done
